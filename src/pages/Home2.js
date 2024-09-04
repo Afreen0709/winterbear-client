59f603de99631638c89a8f05de170ccb2462a0c0
Afreen0709 <afreentaj306@gmail.com>
2024-09-04 18:33:06 +0530

    2nd-homepage

--- src/pages/Home2.js
+++ src/pages/Home2.js
@@ -19,7 +19,7 @@ import "./innerstyle.css";
 import { message } from "antd";
 import SplashScreen from "../components/SplashScreen";
 import SnsSl from "../components/SnsSl";
-const Home2 = () => {
+const Home = () => {
   const [messageApi, contextHolder] = message.useMessage();
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -179,11 +179,242 @@ const Home2 = () => {
     <>
       {contextHolder}
       <Header />
-      {data && data.banners && <HomeSlider />}
+      {/* {data && data.banners &&
+      <HomeSlider />
+      } */}
       {/* <div className="pt-md-5">{data && data.Brands &&
        <BrandSlider />
        }</div> */}
-      <section>
+
+<section className="py-5 mt-4">
+      <div className="container-fluid">
+        <h1 className="text-center">TOP BRANDS</h1>
+        <div className="category-slider">
+          {data &&
+            data.Brands &&
+            data.Brands.map((brand) => (
+              <div className="category-item" key={brand._id}>
+                <div
+                  className="collection-box-1 d-block mx-auto"
+                  onClick={() => handleNavigation(brand._id)}
+                >
+                  <div className="image-container-1">
+                    <img
+                      src={brand.imageUrl || "assets/images/placeholder.png"}
+                      className="mb-0"
+                      alt={brand.name || "Brand Name"}
+                      loading="lazy"
+                      style={{ objectFit: "contain" }}
+                      width="60px"
+                      height="60px"
+                    />
+                    {/* <h4>{brand.name || "Brand"}</h4> */}
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
+    </section>
+    <section className="py-5 shop">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="section-heading">
+                <h1 className="text-start ms-lg-3">OUR CHOICE</h1>
+              </div>
+            </div>
+          </div>
+          <div className="row justify-content-center">
+            {productOldlist &&
+              productOldlist.productList &&
+              productOldlist.productList.slice(0, 1).map((item, index) => {
+                if (!item || !item.products || item.products.length === 0)
+                  return null;
+                return (
+                  <div key={`stickyBody${index}`} className="row">
+                    <div className="col-md-2 g-0 bg-transparent sticky d-none">
+                      <div className="sticky-logo-2">
+                        <img
+                          src={`${item.brand.imageUrl}`}
+                          className="w-75 d-block mx-auto"
+                          alt={item.brand.name}
+                          loading="lazy"
+                        />
+                      </div>
+                    </div>
+                    {/* <div className="mb-3" id="sticky2">
+                      <div
+                        className="sticky-logo-2"
+                        style={{ cursor: "pointer" }}
+                        onClick={() => navigate(`/brand/${item.brand._id}`)}
+                      >
+                        <img
+                          src={`${item.brand.imageUrl}`}
+                          className="d-block mx-auto sticky-log2"
+                          alt={item.brand.name}
+                          loading="lazy"
+                        />
+                      </div>
+                    </div> */}
+                    <div
+                      className="container-fluid shop-by tile-1 px-lg-4 px-0"
+                      id={`sticky1${index}`}
+                    >
+                      {/* <div className="container-fluid">
+                        <ul
+                          className="nav nav-pills nav-fill shop-by-tab py-3 mb-4"
+                          id="pills-tab"
+                          role="tablist"
+                        >
+                          {item.subbrand.map((itemname) => (
+                            <li
+                              className="nav-item py-0"
+                              role="presentation"
+                              key={itemname._id}
+                              onClick={() =>
+                                navigate(`/brand/${item.brand._id}`)
+                              }
+                            >
+                              <img
+                                src={`${itemname.imageUrl}`}
+                                className="shopby-top-icoimg"
+                                alt={itemname.name}
+                                loading="lazy"
+                              />
+                              <button
+                                className="nav-link fw-semibold py-0 position-relative rounded-pill"
+                                id="pills-koya-tab"
+                                data-bs-toggle="pill"
+                                data-bs-target="#pills-koya"
+                                type="button"
+                                role="tab"
+                                aria-controls="pills-koya"
+                                aria-selected="true"
+                              >
+                                {itemname.name}
+                              </button>
+                            </li>
+                          ))}
+                        </ul>
+                      </div> */}
+                      <div className="portfolio mx-lg-3 ml-3 row flex-nowrap overflow-auto">
+                        {item.products
+                          .slice(0, visibleProducts[item.brand._id] || 8)
+                          .map((prod, ind) => (
+                            <div
+                              className="item1 col-lg-3 col-6 position-relative mb-3 home-product px-0 px-4"
+                              onMouseEnter={() => setHoveredProductId(prod._id)}
+                              onMouseLeave={() => setHoveredProductId(null)}
+                              key={ind}
+                            >
+                              <div className="home-product-in">
+                                <img
+                                  src={
+                                    hoveredProductId === prod._id &&
+                                    prod.images.length > 1 &&
+                                    prod.images[1]
+                                      ? prod.images[1]
+                                      : prod.images[0] !== null &&
+                                        prod.images[0] !== "image_url1"
+                                      ? prod.images[0]
+                                      : "assets/images/Rectangle 22.png"
+                                  }
+                                  className="product-shopby img-fluid"
+                                  alt={prod.name}
+                                  loading="lazy"
+                                  onClick={() => handleNavigation(prod._id)}
+                                />
+
+                                <div
+                                  className="text-center border-secondary addtocart-btn px-1 py-1"
+                                  style={{ cursor: "pointer" }}
+                                  onClick={() => addcard(prod)}
+                                >
+                                  <i className="fas fa-cart-plus me-2" /> Add to
+                                  Cart
+                                </div>
+                              </div>
+
+                              <div className="col-md-12 d-flex justify-content-between align-items-end mb-2">
+                                <div className="d-flex justify-content-between position-absolute top-0 start-0 w-100 px-4 px-lg-0">
+                                  {item.brand._id ===
+                                    "65aa405f6bfadce6d5a0ef3c" && (
+                                    <p className="text-white text-center text-decoration-line-through w-25 mt-2 rounded-end bg-theme-dis">
+                                      40%
+                                    </p>
+                                  )}
+                                  <div></div>
+                                  <button
+                                    className="heart-btn"
+                                    id="hertbtn"
+                                    onClick={() => {
+                                      handleWishlists(prod._id);
+                                    }}
+                                  >
+                                    {wishlist?.wishlistItems?.some(
+                                      (item) => item.productId === prod._id
+                                    ) ? (
+                                      <HeartButton isActives={true} />
+                                    ) : (
+                                      <HeartButton isActives={false} />
+                                    )}
+                                  </button>
+                                </div>
+
+                                <div className="mt-4 col-md-12 price-prodname">
+                                  <p className="text-start prize-size mb-0">
+                                    {prod.name}
+                                  </p>
+                                  <p className="prod-pric mb-0">
+                                    ₹{prod.amount}
+                                  </p>
+                                </div>
+                              </div>
+                            </div>
+                          ))}
+                      </div>
+
+                      {/* {!visibleProducts[item.brand._id] ? (
+                        <div
+                          className="text-center view-more"
+                          style={{ cursor: "pointer" }}
+                          onClick={() => loadMoreOrLessProducts(item.brand._id)}
+                        >
+                          View More <br />
+                          <i className="fa-solid fa-angle-down"></i>
+                        </div>
+                      ) : (
+                        <div
+                          className="text-center view-more"
+                          style={{ cursor: "pointer" }}
+                        >
+                          <div
+                            onClick={() =>
+                              loadMoreOrLessProducts(item.brand._id)
+                            }
+                          >
+                            View Less <br />
+                            <i className="fa-solid fa-angle-up"></i>
+                          </div>
+
+                          <div
+                            className="btn text-decoration-none btn-outline-dark fs-6"
+                            onClick={() => loadAllProducts(item.brand._id)}
+                          >
+                            View All <br />
+                          </div>
+                        </div>
+                      )} */}
+                    </div>
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+      </section>
+
+      {/* <section>
         <div className="container">
           \
           <div className="row d-flex justify-content-center my-4 cl-blue py-3">
@@ -222,8 +453,7 @@ const Home2 = () => {
             </div>
           </div>
         </div>
-      </section>
-    
+      </section> */}
 
       <section>{/* <SnsSl /> */}</section>
 
@@ -419,11 +649,16 @@ const Home2 = () => {
           <div className="row">
             <div className="col-md-12">
               <div className="section-heading">
-                <h1 className="text-center">NEW ARRIVAL</h1>
+                
               </div>
             </div>
           </div>
-          <div className="row justify-content-center">
+          <div className="row d-flex justify-content-evenly align-items-center py-2" style={{backgroundColor:"#dbeff2"}}>
+          <h1 className="text-start ms-lg-4">MYSTERY BOX</h1>
+            <div className="col-4">
+                <img className="w-100 h-75" src="../assets/images/bag1.png" />
+            </div>
+            <div className="col-5"> <div className="row justify-content-center">
             {productOldlist &&
               productOldlist.productList &&
               productOldlist.productList.slice(0, 1).map((item, index) => {
@@ -456,7 +691,7 @@ const Home2 = () => {
                       </div>
                     </div> */}
                     <div
-                      className="container-fluid shop-by tile-1 px-lg-4 px-0"
+                      className="container-fluid shop-by tile-11 px-lg-4 px-0"
                       id={`sticky1${index}`}
                     >
                       {/* <div className="container-fluid">
@@ -501,12 +736,12 @@ const Home2 = () => {
                           .slice(0, visibleProducts[item.brand._id] || 8)
                           .map((prod, ind) => (
                             <div
-                              className="item1 col-lg-3 col-6 position-relative mb-3 home-product px-0 px-4"
+                              className="item3 col-lg-3 col-6 position-relative mb-3 home-product px-0 px-4"
                               onMouseEnter={() => setHoveredProductId(prod._id)}
                               onMouseLeave={() => setHoveredProductId(null)}
                               key={ind}
                             >
-                              <div className="home-product-in">
+                              <div className="home-product-in1">
                                 <img
                                   src={
                                     hoveredProductId === prod._id &&
@@ -518,7 +753,7 @@ const Home2 = () => {
                                       ? prod.images[0]
                                       : "assets/images/Rectangle 22.png"
                                   }
-                                  className="product-shopby img-fluid"
+                                  className="product-shopby1 img-fluid"
                                   alt={prod.name}
                                   loading="lazy"
                                   onClick={() => handleNavigation(prod._id)}
@@ -543,7 +778,7 @@ const Home2 = () => {
                                     </p>
                                   )}
                                   <div></div>
-                                  <button
+                                  {/* <button
                                     className="heart-btn"
                                     id="hertbtn"
                                     onClick={() => {
@@ -557,7 +792,7 @@ const Home2 = () => {
                                     ) : (
                                       <HeartButton isActives={false} />
                                     )}
-                                  </button>
+                                  </button> */}
                                 </div>
 
                                 <div className="mt-4 col-md-12 price-prodname">
@@ -608,7 +843,9 @@ const Home2 = () => {
                   </div>
                 );
               })}
-          </div>
+          </div></div>
+            </div>
+         
         </div>
       </section>
 
@@ -843,4 +1080,4 @@ const Home2 = () => {
   );
 };
 
-export default Home2;
+export default Home;
